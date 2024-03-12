cee2839159ac90c585da918c3f97be20db3a0815
Mondmarmelade <91614367+Mondmarmelade@users.noreply.github.com>
2024-03-12 21:53:39 +0100

    feat: :sparkles: Adds leaflet map
    Adds leafletjs and React Leaflet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <p>Hello World!</p>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100vh", width: "wh" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </>
   );
 }
